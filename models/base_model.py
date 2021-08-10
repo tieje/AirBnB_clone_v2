cd6b9aeb992f475954fe2aabf8dc7c1870a74347
tieje <toj320@gmail.com>
2021-08-10 19:10:33 -0400

    Fixed base model. Forgot to add class attributes to base model

--- models/base_model.py
+++ models/base_model.py
@@ -3,12 +3,17 @@
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
