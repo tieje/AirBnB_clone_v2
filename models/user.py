637dce533a1ad45de18236982a1fbfedb628c2dc
keysmusician <justin.masayda@gmail.com>
2021-08-10 23:13:43 +0000

    Fix null requirements; update docstrings

--- models/user.py
+++ models/user.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Defines the class `User`"""
+"""`User` class definition."""
 from sqlalchemy.sql.sqltypes import String
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column
 
 
 class User(BaseModel, Base):
-    """Define a HBnB user."""
+    """A HBnB user."""
     __tablename__ = "users"
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
