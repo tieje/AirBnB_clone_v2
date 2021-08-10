8c2553e103a3945498f40d64a4175c2b4c6aa0cc
tieje <toj320@gmail.com>
2021-08-10 19:18:39 -0400

    Added parentheses to state in __tablename__

--- models/state.py
+++ models/state.py
@@ -7,7 +7,7 @@ from models.city import City
 
 class State(BaseModel):
     """ State class """
-    __tablename__ = states
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship('City', cascade="all, delete", backref='state')
     @property
