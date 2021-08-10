214afcdef328a4eda06c23db9314f8d3cadb74ca
tieje <toj320@gmail.com>
2021-08-10 19:24:50 -0400

    updated __init__ storage to dbstorage

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 
 
-storage = FileStorage()
+storage = DBStorage()
 storage.reload()
