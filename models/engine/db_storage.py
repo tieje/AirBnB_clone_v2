5435f56eaee03dbb9faf660f568ead5bc9ca7b22
tieje <toj320@gmail.com>
2021-08-10 19:33:15 -0400

    getenv() added to hbnb test variable

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -33,7 +33,7 @@ class DBStorage:
                     getenv("HBNB_MYSQL_HOST"),
                     getenv("HBNB_MYSQL_DB")),
             pool_pre_ping=True)
-        if HBNB_ENV == 'test':
+        if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
