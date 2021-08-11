24227924544a045574cb9fd03fafb232a2df2dcf
keysmusician <justin.masayda@gmail.com>
2021-08-11 06:39:32 +0000

    Update documentation

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""
+Determines which storage engine will be used (database or file storage).
+The storage engine can be selected from the environment variable
+`HBNB_TYPE_STORAGE`.
+
+The file storage engine works by converting instances to/from JSON strings and
+reading/writing them to/from a file.
+For details, see "./engine/file_Storage.py".
+
+Alternatively, if database storage engine is selected, it connects to a
+database server and submits queries to manage instances.
+For details, see "./engine/db_storage.py".
+
+"""
 from os import getenv
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
