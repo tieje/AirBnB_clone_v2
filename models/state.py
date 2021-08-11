4aa0bb54b805fe97b478cb19747a5b175a2eee9c
tieje <toj320@gmail.com>
2021-08-10 21:16:45 -0400

    forgot to add Base to models

--- models/state.py
+++ models/state.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship, backref
 from models.city import City
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
