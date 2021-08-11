72e3788fcf8a7799f7e62ccafbe5d6daa9ddfe42
keysmusician <justin.masayda@gmail.com>
2021-08-11 07:10:01 +0000

    Remove unused import

--- models/state.py
+++ models/state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
 
