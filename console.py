45ed2f1702579487496bb4de5bbaf32bd10560ed
tieje <toj320@gmail.com>
2021-08-11 00:10:36 -0400

    I think this will work

--- console.py
+++ console.py
@@ -214,8 +214,8 @@ class HBNBCommand(cmd.Cmd):
         else:
             for k, v in storage.all().items():
                 print_list.append(v)
-
-        print(str(print_list))
+        
+        print(', '.join(map(str, print_list)))
 
     def help_all(self):
         """ Help information for the all command """
