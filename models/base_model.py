7dba6969b1486b734db3ebd8118c0d49f9529ea7
keysmusician <justin.masayda@gmail.com>
2021-08-11 19:00:18 +0000

    Fix  to support new instantiation method

--- models/base_model.py
+++ models/base_model.py
@@ -21,17 +21,21 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiate a new model."""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            # Set each keyword argument as an instance attribute
+            for k, v in kwargs.items():
+                # Do not set `__class__`, since it is set automatically
+                if k == '__class__':
+                    continue
+
+                # Convert datetime strings to datetime objects
+                if k in ('updated_at', 'created_at'):
+                    k = datetime.strptime(k, '%Y-%m-%dT%H:%M:%S.%f')
+
+                setattr(self, k, v)
 
     def __str__(self):
         """Return a string representation of the instance."""
