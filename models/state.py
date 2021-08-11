7cbcd5b2025a75836851ed66f9f8f3e85de97f5a
keysmusician <justin.masayda@gmail.com>
2021-08-11 00:56:57 +0000

    Edit docstrings, cleanup imports

--- models/state.py
+++ models/state.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """`State` class definition."""
 from models.base_model import BaseModel
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.city import City
 
 
 class State(BaseModel):
-    """ State class """
+    """A US state."""
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship('City', cascade="all, delete", backref='state')
 
     @property
     def cities(self):
-        '''Returns the list of City instances with state_id'''
+        """Return the list of `City` instances with `state_id`."""
         from models import storage
         citiesOState = []
         for c in storage.all(City).values():
