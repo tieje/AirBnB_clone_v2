3a6c8c265783905bf34a845560ee5ceaa0048ccd
tieje <toj320@gmail.com>
2021-08-11 00:16:13 -0400

    there

--- console.py
+++ console.py
@@ -215,7 +215,7 @@ class HBNBCommand(cmd.Cmd):
             for k, v in storage.all().items():
                 print_list.append(v)
 
-        print(print_list)
+        print([', '.map(str, print_list)])
 
     def help_all(self):
         """ Help information for the all command """
