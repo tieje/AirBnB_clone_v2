5e671d04e944bd02591282726a57dc7d6517f002
keysmusician <justin.masayda@gmail.com>
2021-08-11 18:09:35 +0000

    Update docstring; Use list comprehention implementation in  property

--- models/state.py
+++ models/state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""`State` class definition."""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -7,17 +7,13 @@ from models.city import City
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """A U.S. state."""
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship('City', cascade="all, delete", backref='state')
 
     @property
     def cities(self):
-        """Return the list of `City` instances with `state_id`."""
-        from models import storage
-        citiesOState = []
-        for c in storage.all(City).values():
-            if c.state_id == self.id:
-                citiesOState.append(c)
-        return citiesOState
+        """Return the list of cities matching this state."""
+        return [city for city in storage.all(City).values()
+                if city.state_id == self.id]
