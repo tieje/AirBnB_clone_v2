a6c33d97a6f9e8e2802ba532043b42c361136d9f
tieje <toj320@gmail.com>
2021-08-25 21:04:45 -0400

    there

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+sudo apt-get -y update
+sudo apt-get -y install nginx
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+touch /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+ubuntu@89-web-01:~/$ curl localhost/hbnb_static/index.html
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current 
+chown -R ubuntu:ubuntu /data/
+
+
+service nginx restart
