1430dd1a6d9041e62b1e40e5b19d47e97526f568
tieje <toj320@gmail.com>
2021-09-09 17:23:29 -0400

    1 and addition of comments

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+'''Return Hello HBNB'''
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello():
+    '''Return Hello HBNB'''
     return 'Hello, HBNB!'
 
 if __name__ == '__main__':
---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''An addition that returns just HBNB'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello, HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''An addition that returns just HBNB'''
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run()
