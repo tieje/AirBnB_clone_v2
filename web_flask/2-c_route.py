23c777da0bb5936ffa0a0ee8fd45c6e3e098c228
tieje <toj320@gmail.com>
2021-09-11 12:36:43 -0400

    2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,7 +20,7 @@ def hbnb():
 def displayText(text):
     '''Displays the text variable'''
     text = text.replace("_", " ")
-    return f'C {text}'
+    return 'C {}'.format(text)
 
 
 if __name__ == '__main__':
