79bdd796b57403f8aa974b681a60a8ba72a60ea4
tieje <toj320@gmail.com>
2021-09-11 12:31:03 -0400

    hm

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     '''Return Hello HBNB'''
-    return 'Hello, HBNB!'
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
