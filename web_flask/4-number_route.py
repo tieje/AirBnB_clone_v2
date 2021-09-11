f6e70bcfa8790b5f64c8066c800ba537705c2300
tieje <toj320@gmail.com>
2021-09-11 12:56:15 -0400

    4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+'''An addition that returns just HBNB'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''An addition that returns just HBNB'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    '''Displays the text variable'''
+    text = text.replace("_", " ")
+    return 'C {}'.format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def displayPythonText(text='is cool'):
+    '''Displays the text variable but with python'''
+    display_text = text.replace('_', ' ')
+    return 'Python {}'.format(display_text)
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def numberdisplay(n):
+    '''Displays the number'''
+    try:
+        n = int(n)
+        return '{} is a number'.format(n)
+    except:
+        return
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
